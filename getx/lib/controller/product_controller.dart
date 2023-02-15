0b7a4a0c02eee6abf80c0e6f709754cfd2e3b8a4
tech-aziz <aziz.swe71@gmail.com>
2023-02-14 18:01:28 -0800

    some changes file

--- getx/lib/controller/product_controller.dart
+++ getx/lib/controller/product_controller.dart
@@ -1,45 +1,46 @@
-import 'package:get/get.dart';
-import 'package:getx/model/product.dart';
+// import 'package:get/get.dart';
+// import 'package:getx/model/product.dart';
 
-class ProductController extends GetxController {
-  // List<Product> products = [];
-  List<Product> product = <Product>[].obs;
-  @override
-  void onInit() {
-    // TODO: implement onInit
-    super.onInit();
-    fetchProducts();
-  }
+// class ProductController extends GetxController {
+//   // List<Product> products = [];
+//   List<Product> product = <Product>[].obs;
+//   @override
+//   void onInit() {
+//     // TODO: implement onInit
+//     super.onInit();
+//     fetchProducts();
+//   }
 
-  void fetchProducts() async {
-    await Future.delayed(const Duration(
-      seconds: 1,
-    ));
-    var serverResponse = [
-      Product(
-          id: 0,
-          productName: 'Coca-cala',
-          price: 202,
-          productImage: '',
-          productDescription: 'This is the description of the product'),
-      Product(
-          id: 1,
-          price: 30,
-          productDescription: 'some description about product',
-          productImage: 'abd',
-          productName: 'FirstProd'),
-      Product(
-          id: 2,
-          price: 40,
-          productDescription: 'some description about product',
-          productImage: 'abd',
-          productName: 'SecProd'),
-      Product(
-          id: 3,
-          price: 49.5,
-          productDescription: 'some description about product',
-          productImage: 'abd',
-          productName: 'ThirdProd'),
-    ];
-  }
-}
+//   void fetchProducts() async {
+//     await Future.delayed(const Duration(
+//       seconds: 1,
+//     ));
+//     var serverResponse = [
+//       Product(
+//           id: 0,
+//           productName: 'Coca-cala',
+//           price: 202,
+//           productImage: '',
+//           productDescription: 'This is the description of the product'),
+//       Product(
+//           id: 1,
+//           price: 30,
+//           productDescription: 'some description about product',
+//           productImage: 'abd',
+//           productName: 'FirstProd'),
+//       Product(
+//           id: 2,
+//           price: 40,
+//           productDescription: 'some description about product',
+//           productImage: 'abd',
+//           productName: 'SecProd'),
+//       Product(
+//           id: 3,
+//           price: 49.5,
+//           productDescription: 'some description about product',
+//           productImage: 'abd',
+//           productName: 'ThirdProd'),
+//     ];
+//     product = serverResponse;
+//   }
+// }
